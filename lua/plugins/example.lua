295855e7abad560161e2b976be064cdff2dcf113
Cameron <cameron.wolff@proton.me>
2025-10-12 14:10:05 -0700

    set colorscheme

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -10,7 +10,7 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
 
   -- Configure LazyVim to load gruvbox
   {
